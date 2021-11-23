b1b7075887437d447f66034b6e5717cbc734a1c4
dinah <cybergirldinah@gmail.com>
2021-11-22 23:59:13 -0600

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Container className="p-3">
+      <Container className="p-5 mb-4 bg-light rounded-3">
+        <Card border="primary" style={{ width: '20rem' }}>
+          <Card.Body>Balance</Card.Body>
+          <InputGroup className="mb-3">
+            <FormControl aria-label="Text input with dropdown button" />
+              <DropdownButton
+                variant="outline-secondary"
+                title="Select"
+                id="input-group-dropdown-2"
+                align="end"
+              >
+                <Dropdown.Item href="#">Deposit</Dropdown.Item>
+                <Dropdown.Item href="#">Withdraw</Dropdown.Item>
+              </DropdownButton>
+          </InputGroup>
+        </Card>  
+      </Container>
+  </Container>
+   
   );
 }
 
