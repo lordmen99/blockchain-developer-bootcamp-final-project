72476215dbeddfe15c3d23621661c2e84aa22326
dinah <cybergirldinah@gmail.com>
2021-11-23 00:42:22 -0600

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,32 +5,58 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-   
     <Container className="p-3">
-      <Container className="p-5 mb-4 bg-light rounded-3">
-        <Card border="primary" style={{ width: '20rem' }}>
+      <Container >
+        <Row className="justify-content-sm-center mb-5">
+          <Col sm={8}></Col>
+          <Col sm={4}>
+            <Button bg-primary>Connect Wallet</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+        <Card className="p-2 m-auto mb-5 text-center" border="primary" style={{ width: '28rem' }}>
           <Card.Body>Balance</Card.Body>
-          <InputGroup className="mb-3">
-            <FormControl aria-label="Text input with dropdown button" />
-              <DropdownButton
-                variant="outline-secondary"
-                title="Select"
-                id="input-group-dropdown-2"
-                align="end"
-              >
-                <Dropdown.Item href="#">Deposit</Dropdown.Item>
-                <Dropdown.Item href="#">Withdraw</Dropdown.Item>
-              </DropdownButton>
+          <InputGroup className="mb-3 p-2">
+            <FormControl
+              placeholder="Amount"
+              aria-label="Amount"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-primary" id="button-addon2" style={{ width: '6rem' }}>
+              Deposit
+            </Button>
           </InputGroup>
-        </Card>  
+          <InputGroup className="mb-3 p-2">
+            <FormControl
+              placeholder="Amount"
+              aria-label="Amount"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2" style={{ width: '6rem' }}>
+              Withdraw
+            </Button>
+          </InputGroup>
+        </Card>
+        </Col>
+        </Row>
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Button>Claim</Button>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
   </Container>
-   
   );
 }
 
